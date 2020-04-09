6c32606c275be7491753d03168a0da4749e7f3a6
Anders Kristiansen <anders.kristiansen@nav.no>
2020-04-09 09:02:17 +0200

    Beholder gamle regler i tilfelle

---
+++ src/components/OverføringAdvarsel__GamleRegler.test.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import OverføringAdvarselGamleRegler from './OverføringAdvarsel__GamleRegler';
+import Overføringsdager from '../types/Overføringsdager';
+import Omsorgsprinsipper from '../types/Omsorgsprinsipper';
+
+const grunnrettOgAleneomsorg: Omsorgsprinsipper = {
+  grunnrett: {
+    normaldager: 10,
+    koronadager: 10,
+  },
+  aleneomsorg: {
+    normaldager: 10,
+    koronadager: 10,
+  },
+  kroniskSykt: {
+    koronadager: 0,
+    normaldager: 0,
+  },
+  aleneomsorgKroniskSyke: {
+    koronadager: 0,
+    normaldager: 0,
+  },
+};
+
+test('Overført flere normaldager enn tilgjengelig gir advarsel', () => {
+  const overføringsdager: Overføringsdager = {
+    fordelteNormaldager: 11,
+    mottatteNormaldager: 0,
+    overførteKoronadager: 0,
+    mottatteKoronadager: 0,
+  };
+
+  const { getByText } = render(
+    <OverføringAdvarselGamleRegler omsorgsprinsipper={grunnrettOgAleneomsorg} overføringsdager={overføringsdager} />,
+  );
+  const advarsel = getByText(/Brukeren har overført\/fordelt/);
+
+  expect(advarsel).toBeDefined();
+});
+
+test('Overført flere koronadager enn tilgjengelig gir advarsel', () => {
+  const overføringsdager: Overføringsdager = {
+    fordelteNormaldager: 0,
+    mottatteNormaldager: 0,
+    overførteKoronadager: 21,
+    mottatteKoronadager: 0,
+  };
+  const { getByText } = render(
+    <OverføringAdvarselGamleRegler omsorgsprinsipper={grunnrettOgAleneomsorg} overføringsdager={overføringsdager} />,
+  );
+
+  const advarsel = getByText(/Brukeren har overført 21 koronadager/);
+
+  expect(advarsel).toBeDefined();
+});
---
+++ src/components/OverføringAdvarsel__GamleRegler.tsx
@@ -0,0 +1,51 @@
+import React, { FunctionComponent, useMemo } from 'react';
+import Omsorgsprinsipper from '../types/Omsorgsprinsipper';
+import Overføringsdager from '../types/Overføringsdager';
+import AlertStripe from 'nav-frontend-alertstriper';
+import tekster from '../tekster';
+
+interface OverføringAdvarselProps {
+  omsorgsprinsipper: Omsorgsprinsipper;
+  overføringsdager: Overføringsdager;
+}
+
+const OverføringAdvarsel__GamleRegler: FunctionComponent<OverføringAdvarselProps> = ({
+  omsorgsprinsipper,
+  overføringsdager,
+}) => {
+  const antallKoronadager = useMemo<number>(
+    () => Object.values(omsorgsprinsipper).reduce((sum, dag) => sum + dag.koronadager, 0),
+    [omsorgsprinsipper],
+  );
+  const { aleneomsorg, aleneomsorgKroniskSyke } = omsorgsprinsipper;
+  const aleneomsorgsdager = useMemo(() => aleneomsorgKroniskSyke.normaldager + aleneomsorg.normaldager, [
+    aleneomsorg,
+    aleneomsorgKroniskSyke,
+  ]);
+
+  const harOverførtFlerNormaldagerEnnTilgjengelig = overføringsdager.fordelteNormaldager > aleneomsorgsdager;
+  const harOverførtFlerKoronadagerEnnTilgjengelig = overføringsdager.overførteKoronadager > antallKoronadager;
+
+  return (
+    <>
+      {harOverførtFlerNormaldagerEnnTilgjengelig && (
+        <AlertStripe type="advarsel">
+          {tekster('Resultat.AdvarselNormal', {
+            overførteDager: `${overføringsdager.fordelteNormaldager}`,
+            tilgjengeligeDager: `${aleneomsorgsdager}`,
+          })}
+        </AlertStripe>
+      )}
+      {harOverførtFlerKoronadagerEnnTilgjengelig && (
+        <AlertStripe type="advarsel">
+          {tekster('Resultat.AdvarselKorona', {
+            overførteDager: `${overføringsdager.overførteKoronadager}`,
+            tilgjengeligeDager: `${antallKoronadager}`,
+          })}
+        </AlertStripe>
+      )}
+    </>
+  );
+};
+
+export default OverføringAdvarsel__GamleRegler;
