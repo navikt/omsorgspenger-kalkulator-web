77538f0ecf65a720bdcce246219db16de3f7bb6f
eras-dev <emma.rasmusson@nav.no>
2022-01-07 11:36:52 +0100

    lägger til react-scripts igen då den failer utan och uppdaterar snapshots i test

--- src/components/Resultat.test.tsx
+++ src/components/Resultat.test.tsx
@@ -8,81 +8,78 @@ import Omsorgsdager from '@navikt/kalkuler-omsorgsdager/lib/types/Omsorgsdager';
 import OmsorgsdagerForm from '@navikt/kalkuler-omsorgsdager/lib/types/OmsorgsdagerForm';
 import PeriodeEnum from '@navikt/kalkuler-omsorgsdager/lib/types/PeriodeEnum';
 
-describe('Resultat', () => {
-  test('Summerer alle omsorgsprinsipper', () => {
-    const omsorgsprinsipper: Omsorgsprinsipper = {
-      grunnrett: {
-        normaldager: 15,
-        koronadager: 15,
-      },
-      kroniskSykt: {
-        normaldager: 10,
-        koronadager: 10,
-      },
-      aleneomsorg: {
-        normaldager: 10,
-        koronadager: 10,
-      },
-      aleneomsorgKroniskSyke: {
-        normaldager: 20,
-        koronadager: 20,
-      },
-    };
+test('Summerer alle omsorgsprinsipper', () => {
+  const omsorgsprinsipper: Omsorgsprinsipper = {
+    grunnrett: {
+      normaldager: 15,
+      koronadager: 15,
+    },
+    kroniskSykt: {
+      normaldager: 10,
+      koronadager: 10,
+    },
+    aleneomsorg: {
+      normaldager: 10,
+      koronadager: 10,
+    },
+    aleneomsorgKroniskSyke: {
+      normaldager: 20,
+      koronadager: 20,
+    },
+  };
 
-    const overføringsdager: Omsorgsdager = {
-      koronadager: -12,
-      normaldager: -4,
-    };
-    const sum = summerDager(...Object.values(omsorgsprinsipper), overføringsdager);
+  const overføringsdager: Omsorgsdager = {
+    koronadager: -12,
+    normaldager: -4,
+  };
+  const sum = summerDager(...Object.values(omsorgsprinsipper), overføringsdager);
 
-    expect(sum).toEqual(94);
-  });
-
-  test('Rendrer riktig resultat', () => {
-    const initValues: OmsorgsdagerForm = {
-      barn: treBarnEttKroniskOgAleneomsorg,
-      foreldre: [],
-      periode: PeriodeEnum.Koronaperiode,
-    };
-
-    const { asFragment } = render(
-      <SkjemaContext initialValues={initValues}>
-        <Resultat />
-      </SkjemaContext>,
-    );
-
-    expect(asFragment()).toMatchSnapshot();
-  });
-
-  test('Rendrer advarsel hvis man overfører fler dager enn man kan', () => {
-    const initValues: OmsorgsdagerForm = {
-      barn: [barnUnder12],
-      foreldre: [
-        {
-          id: '1',
-          normaldager: {
-            dagerFått: 0,
-            dagerTildelt: 11,
-          },
-          koronadager: {
-            dagerTildelt: 11,
-            dagerFått: 0,
-          },
-        },
-      ],
-      periode: PeriodeEnum.Koronaperiode,
-    };
-
-    const { asFragment } = render(
-      <SkjemaContext initialValues={initValues}>
-        <Resultat />
-      </SkjemaContext>,
-    );
-
-    expect(asFragment()).toMatchSnapshot();
-  });
+  expect(sum).toEqual(94);
 });
 
+test('Rendrer riktig resultat', () => {
+  const initValues: OmsorgsdagerForm = {
+    barn: treBarnEttKroniskOgAleneomsorg,
+    foreldre: [],
+    periode: PeriodeEnum.Koronaperiode,
+  };
+
+  const { asFragment } = render(
+    <SkjemaContext initialValues={initValues}>
+      <Resultat />
+    </SkjemaContext>,
+  );
+
+  expect(asFragment()).toMatchSnapshot();
+});
+
+test('Rendrer advarsel hvis man overfører fler dager enn man kan', () => {
+  const initValues: OmsorgsdagerForm = {
+    barn: [barnUnder12],
+    foreldre: [
+      {
+        id: '1',
+        normaldager: {
+          dagerFått: 0,
+          dagerTildelt: 11,
+        },
+        koronadager: {
+          dagerTildelt: 11,
+          dagerFått: 0,
+        },
+      },
+    ],
+    periode: PeriodeEnum.Koronaperiode,
+  };
+
+  const { asFragment } = render(
+    <SkjemaContext initialValues={initValues}>
+      <Resultat />
+    </SkjemaContext>,
+  );
+
+  expect(asFragment()).toMatchSnapshot();
+});
 
 describe('Visning av Resultat med/uten koronafelter', () => {
   const rendered = (periode: PeriodeEnum): RenderResult => {
