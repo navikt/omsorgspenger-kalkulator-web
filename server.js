e1f172c81767f92de22c2f5116d51ac72a85a57e
eras-dev <emma.rasmusson@nav.no>
2022-01-13 10:30:41 +0100

    Revert "lagt in rate limit i server for Snyk varsel (#29)"
    This reverts commit 825dad133d1efda0778d83af61e3ada5f61b0a78.

--- server.js
+++ server.js
@@ -1,18 +1,6 @@
 const express = require('express');
-const rateLimit = require('express-rate-limit');
 const path = require('path');
 const server = express();
-const cookieParser = require('cookie-parser')
-
-const limiter = rateLimit({
-  windowMs: 60 * 60 * 1000, // 1 hour
-  max: 5, 
-  message: 'Før mange requests.',
-  standardHeaders: true,
-  legacyHeaders: false, 
-})
-
-server.use(cookieParser);
 server.use(express.static(path.join(__dirname, 'build')));
 
 const PORT = process.env.PORT || 8080;
@@ -20,7 +8,7 @@ const PORT = process.env.PORT || 8080;
 server.get('/isAlive', (req, res) => res.sendStatus(200));
 server.get('/isReady', (req, res) => res.sendStatus(200));
 
-server.get('/', limiter, function(req, res) {
+server.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
