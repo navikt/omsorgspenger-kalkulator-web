44ec20c75d100fbe70f72aec94ce11d8daaf6496
eras-dev <emma.rasmusson@nav.no>
2022-01-13 10:30:29 +0100

    Revert "fjernet cookie-parser"
    This reverts commit 03c63ae32e5a75e420842ea473b504edf510f7df.

--- server.js
+++ server.js
@@ -2,15 +2,17 @@ const express = require('express');
 const rateLimit = require('express-rate-limit');
 const path = require('path');
 const server = express();
+const cookieParser = require('cookie-parser')
 
 const limiter = rateLimit({
-  windowMs: 60 * 60 * 1000,
-  max: 10,
-  message: 'Før mange requests',
+  windowMs: 60 * 60 * 1000, // 1 hour
+  max: 5, 
+  message: 'Før mange requests.',
   standardHeaders: true,
   legacyHeaders: false, 
 })
 
+server.use(cookieParser);
 server.use(express.static(path.join(__dirname, 'build')));
 
 const PORT = process.env.PORT || 8080;
